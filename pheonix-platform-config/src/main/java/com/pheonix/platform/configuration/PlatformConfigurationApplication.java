c90ec7f9b629152fe1b858d03f4e7c8c45bf6b81
praveenit219 <praveen.tirunamali@gmail.com>
2019-02-19 16:42:28 +0800

    pushing config and discovery modules with basic structure

---
+++ pheonix-platform-config/src/main/java/com/pheonix/platform/configuration/PlatformConfigurationApplication.java
@@ -0,0 +1,12 @@
+package com.pheonix.platform.configuration;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class PlatformConfigurationApplication {
+
+}
---
+++ pheonix-platform-discovery/src/main/java/com/pheonix/platform/discovery/PlatformDiscoveryApplication.java
@@ -0,0 +1,9 @@
+package com.pheonix.platform.discovery;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class PlatformDiscoveryApplication {
+
+}
