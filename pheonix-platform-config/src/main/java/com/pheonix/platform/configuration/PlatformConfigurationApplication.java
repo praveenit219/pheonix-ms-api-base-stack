448100118e1af56e845c274405f3e897545eee88
praveenit219 <praveen.tirunamali@gmail.com>
2019-02-20 09:26:34 +0800

    updated eureka and other configs, added security gateway module

--- pheonix-platform-config/src/main/java/com/pheonix/platform/configuration/PlatformConfigurationApplication.java
+++ pheonix-platform-config/src/main/java/com/pheonix/platform/configuration/PlatformConfigurationApplication.java
@@ -1,6 +1,7 @@
 package com.pheonix.platform.configuration;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -8,5 +9,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @EnableDiscoveryClient
 public class PlatformConfigurationApplication {
+
+	 public static void main(String[] args) {
+	        SpringApplication.run(PlatformConfigurationApplication.class, args);
+	    }
 
 }
--- pheonix-platform-discovery/src/main/java/com/pheonix/platform/discovery/PlatformDiscoveryApplication.java
+++ pheonix-platform-discovery/src/main/java/com/pheonix/platform/discovery/PlatformDiscoveryApplication.java
@@ -1,10 +1,15 @@
 package com.pheonix.platform.discovery;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class PlatformDiscoveryApplication {
+
+	 public static void main(String[] args) {
+	        SpringApplication.run(PlatformDiscoveryApplication.class, args);
+	    }
 
 }
